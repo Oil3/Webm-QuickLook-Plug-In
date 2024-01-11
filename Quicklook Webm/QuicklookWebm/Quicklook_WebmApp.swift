b97d5c123e51ca97c1534fabb5b1bc05232aea9a
Morris Quet Almahdi <milehighsol@gmail.com>
2024-01-11 08:55:09 -0500

    Sources as they were 11JAN080024
    It was quite complex setting up the Info.plist, and surely it can be improved further.

---
+++ Quicklook Webm/QuicklookWebm/Quicklook_WebmApp.swift
@@ -0,0 +1,18 @@
+//
+//  Quicklook_WebmApp.swift
+//  Quicklook Webm
+//
+//  Created by ZZS on 08/01/2024.
+//
+
+import SwiftUI
+
+@main
+
+struct Quicklook_WebmApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
