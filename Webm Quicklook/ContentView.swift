562bf02c345e010c6d3786ae0a3e2b1cd6b26129
Oil3 <3oiltree@gmail.com>
2024-02-18 15:37:52 -0500

    Initial Commit

---
+++ Webm Quicklook/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Webm Quicklook
+//
+//  Created by ZZS on 18/02/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Webm Quicklook/Webm_QuicklookApp.swift
@@ -0,0 +1,17 @@
+//
+//  Webm_QuicklookApp.swift
+//  Webm Quicklook
+//
+//  Created by ZZS on 18/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct Webm_QuicklookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
